17fed73061f2c08115b7003e3cf3993d55376d98
Peerapat Surasing <p.surasing@hotmail.com>
2023-08-04 01:19:00 +0700

    New Proj

---
+++ Service-API/Program.cs
@@ -0,0 +1,12 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Service_API;
+
+var builder = Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+builder.Build().Run();
---
+++ Service-API/Startup.cs
@@ -0,0 +1,123 @@
+﻿using Middleware.Shared.Startups;
+using Middleware.Intercom.Protos;
+using Middleware.Intercom.Setting;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Service_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureGeneralServices(services);
+
+            #region Mongo
+
+            #endregion Mongo
+
+            #region SQL
+
+            #endregion SQL
+
+            #region Redis
+
+            #endregion Redis
+
+            #region Services
+
+            services.AddHttpContextAccessor();
+
+            //services.AddScoped<ITimeZoneService, TimeZoneService>();
+
+            #endregion Services
+
+            #region Grcp Services
+
+            var grcpServiceSetting = new GrcpServiceSetting(Configuration);
+
+            services.AddGrpcClient<ConfigProto.ConfigProtoClient>(o =>
+            {
+                o.Address = new Uri(grcpServiceSetting.GetSetting("Config").Url);
+            });
+
+            #endregion Grcp Services
+        }
+
+        public void ConfigureGeneralServices(IServiceCollection services)
+        {
+            #region controllers
+
+            services.AddControllers();
+
+
+            #endregion controllers
+
+            #region log
+
+            #endregion log
+
+            #region ActionContextAccessor
+
+            services.AddScoped<IActionContextAccessor, ActionContextAccessor>();
+
+            #endregion ActionContextAccessor
+
+            #region AutoMapper
+
+
+            #endregion AutoMapper
+
+            #region HttpContextAccessor
+
+            services.AddHttpContextAccessor();
+
+            #endregion HttpContextAccessor
+
+            #region Swagger
+            StartupHelper.CreateSwagger(services);
+
+            #endregion Swagger
+
+            #region grpc
+
+            services.AddGrpc();
+
+            #endregion grpc
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseRouting();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                //grpc
+                //endpoints.MapGrpcService<TemplateService>();
+            });
+        }
+    }
+}
+
